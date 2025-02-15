739d556e21f420e5d160556f6654fa0f740bd887
nnul <salahxd99@gmail.com>
2025-02-15 03:09:56 +0100

    added a bottom nav bar but sill wip also fixed nested git file issues

---
+++ app/src/main/java/com/example/khadra/view/MapScreen.kt
@@ -0,0 +1,24 @@
+package com.example.khadra.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MapScreen(modifier: Modifier = Modifier) {
+    Column (modifier=Modifier.fillMaxSize(),
+        verticalArrangement =  Arrangement.Center,
+        horizontalAlignment =  Alignment.CenterHorizontally)
+    {
+
+        Text("Map Screen", fontSize = 48.sp, fontWeight = FontWeight.ExtraBold)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/khadra/view/NavItem.kt
@@ -0,0 +1,8 @@
+package com.example.khadra.view
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem(
+    val label:String,
+    val icon: ImageVector
+)
