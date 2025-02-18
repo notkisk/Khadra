57b3ae191217900425cf8e39df995c10e243d44e
nnul <salahxd99@gmail.com>
2025-02-18 01:40:36 +0100

    made tree cards strill wip

--- app/src/main/java/com/example/khadra/view/MainScreen.kt
+++ app/src/main/java/com/example/khadra/view/MainScreen.kt
@@ -1,12 +1,10 @@
 package com.example.khadra.view
 
-import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -24,25 +22,23 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.example.khadra.R
 import com.example.khadra.model.NavItem
-import com.example.khadra.model.Tree
 import com.example.khadra.ui.theme.Inter
 import com.example.khadra.ui.theme.KhadraGreen
 import com.example.khadra.ui.theme.KhadraTheme
 import com.example.khadra.viewmodel.TreeViewModel
-import kotlin.math.log
 
 
 @Composable
@@ -162,7 +158,7 @@ fun HomeScreen(modifier: Modifier) {
                 modifier = Modifier.fillMaxWidth().padding(horizontal = 6.dp),
                 shape = RoundedCornerShape(32.dp), // ✅ Matches Box clipping
                 singleLine = true,
-                placeholder = { Text("Search...") },
+                placeholder = { Text("Search...", fontSize = 16.sp, color = Color.Black) },
                 trailingIcon = {
                     Icon(
                         modifier = Modifier.size(42.dp),
@@ -193,9 +189,31 @@ fun HomeScreen(modifier: Modifier) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Box()
+          /*  Card(
+                modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(4.dp),
+                shape = RoundedCornerShape(8.dp), colors = CardColors(containerColor = Color.White
+                    , contentColor = Color.Black, disabledContentColor = Color.White, disabledContainerColor = Color.White)
+            ) {
+                Row () {
+                    Box(modifier = Modifier.weight(0.25f).height(100.dp).border(1.5.dp, color = Color.Black.copy(alpha = 0.25f), shape = RoundedCornerShape(9.dp))){
+
+                    }
+
+                    Box(modifier = Modifier.weight(0.5f).height(100.dp)){
+
+                    }
+                    Box(modifier = Modifier.weight(0.25f).height(100.dp)){
+
+                    }
+
+                }
+            }*/
 
 
+            TreeCard("healthy")
+            Spacer(Modifier.height(16.dp))
+            TreeCard("low")
         }
 
 
@@ -216,11 +234,11 @@ when (selectedIndex){
 
 }
 
-    Surface(
+    Card(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp),
-        color = Color.Black,
-        shadowElevation = 100.dp // Elevation creates a shadow effect
+        //color = Color.Black,
+        elevation = CardDefaults.cardElevation(8.dp) // Elevation creates a shadow effect
     ) {
         Box(
             contentAlignment = Alignment.BottomCenter,
@@ -278,3 +296,163 @@ fun PreviewMainScreen() {
 }
 
 
+@Composable
+fun TreeCard(status:String="healthy") {
+
+    Surface( modifier = Modifier.wrapContentSize().padding(horizontal = 16.dp)
+        .shadow(
+            elevation = 6.dp, // Shadow elevation
+            shape = RoundedCornerShape(10.dp),
+            clip = true, // Clip the shadow to the shape
+        ), shape = RoundedCornerShape(10.dp))
+    {
+        Card(
+
+            border = BorderStroke(1.dp, color = Color.Black.copy(alpha = 0.25f)),
+            shape = RoundedCornerShape(9.dp), colors = CardColors(containerColor = Color(0x00FFFFFF)
+                , contentColor = Color(0xFFFFFFFF), disabledContentColor = Color.White, disabledContainerColor = Color.White)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                // Space 1 (25%)
+                Box(modifier = Modifier.weight(0.25f).height(100.dp).border(1.dp, color = Color.Black.copy(alpha = 0.25f), shape = RoundedCornerShape(9.dp))){
+                    Column (modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+                        Text(
+                            text = ":الحالة ",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            modifier = Modifier
+                                .zIndex(100f).fillMaxWidth().align(Alignment.End),
+                            textAlign = TextAlign.Center // Ensures text alignment within its own bounds
+                        )
+
+                        StatusBar(status)
+
+                    }
+
+
+                }
+
+                // Space 2 (50%)
+                Box(
+                    modifier = Modifier.height(100.dp).fillMaxSize()
+                        .weight(0.5f)
+                ) {
+                    Column (modifier=Modifier.height(100.dp).fillMaxWidth()){
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd){
+                            Text(
+                                text = "شجرة زيتون الاخضر",
+                                fontSize = 20.sp,
+                                color = Color.Black, fontWeight = FontWeight.ExtraBold, lineHeight = 2.sp
+                            )
+                        }
+
+                        Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
+                            Text(
+                                text = " البياضة، الوادي، الوادي",
+                                fontSize = 11.sp,
+                                color = Color.Gray,
+                                textAlign = TextAlign.End, fontWeight = FontWeight.Light
+                            )
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_outline_location_on), // Replace with your icon resource
+                                contentDescription = "Example Icon",
+                                modifier = Modifier.size(20.dp),
+                                tint = Color.Gray // Optional: Set icon color
+                            )
+
+
+                        }
+
+
+                        Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
+                            Text(
+                                text = "هيثم بكاري: 10 شجرة",
+                                fontSize = 11.sp,
+                                fontWeight = FontWeight.Light,
+                                color = Color.Gray
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .size(20.dp) // Set the size of the circular image with border
+                                    .clip(CircleShape).shadow(elevation = 4.dp) // Apply shadow // Clip the Box to a circular shape
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.guy1), // Replace with your image resource
+                                    contentDescription = "Circular Image with Border",
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .clip(CircleShape), // Clip the image to a circular shape
+                                    contentScale = ContentScale.Crop // Ensures the image fills the circle
+                                )
+                            }
+
+
+
+                        }
+                    }
+                }
+
+                // Space 3 (25%)
+                Box(
+                    modifier = Modifier
+                        .weight(0.25f)
+                        .aspectRatio(1f) // Enforce 1:1 aspect ratio for the Box
+                        .wrapContentSize() // Make the Box wrap around the content
+                        .padding(8.dp)
+                        .border(1.dp, color = Color.Black.copy(alpha = 0.25f), shape = RoundedCornerShape(20.dp))
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.tree1), // Replace with your image resource
+                        contentDescription = "Image 2",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Fit // Ensures the image fits inside the Box
+                    )
+                }
+            }
+        }
+    }
+
+
+}
+
+
+@Composable
+fun StatusBar(status: String) {
+    val color = when (status.lowercase()) { // Convert to lowercase for case-insensitive comparison
+        "low" -> Color(0xFFFF0000)
+        "moderate" -> Color(0xFFFFAA00)
+        "healthy" -> KhadraGreen
+        else -> Color.Gray // Default color for unknown status
+    }
+
+    val progress = when (status.lowercase()) { // Convert to lowercase for case-insensitive comparison
+        "low" -> 25
+        "moderate" -> 40
+        "healthy" -> 60
+        else -> 60 // Default color for unknown status
+    }
+
+    Box (contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+
+        Box(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(4.dp))
+                .width(60.dp)
+                .height(10.dp) // Height of the status bar
+                .background(Color.White).border(BorderStroke(1.dp, color = Color.Gray))
+        ){
+            Box(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(4.dp))
+                    .width(progress.dp)
+                    .height(10.dp) // Height of the status bar
+                    .background(color).border(BorderStroke(1.dp, color = Color.Gray))
+            )
+        }
+    }
+
+
+
+}
